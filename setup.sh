80afae76217f254bee996027368cb9ca9b0084ff
Heavybaby5000 <Heavybaby5000@gmail.com>
2020-05-19 21:38:59 +0900

    デプロイ用スクリプトの改良
    - ホームディレクトリ直下にcloneしなくても動作するよう変更
    - 元のdotfilesを消さずに残すよう変更
    - ちょっと自動化

--- setup.sh
+++ setup.sh
@@ -1,12 +1,20 @@
-#!/bin/sh
+#!/bin/bash
+DIR=`cd $(dirname $0) && pwd`
 
 #素朴なやつ
 #あとで自動化するから許せ
-rm ~/.bash_logout
-ln -sf ~/.dotfiles/bash_logout ~/.bash_logout
-rm ~/.bash_profile
-ln -sf ~/.dotfiles/bash_profile ~/.bash_profile
-rm ~/.bashrc
-ln -sf ~/.dotfiles/bashrc ~/.bashrc
-rm ~/.zshrc
-ln -sf ~/.dotfiles/zshrc ~/.zshrc
+DOTFILES=(bash_logout bash_profile bashrc zshrc)
+
+echo [link dotfiles]
+echo -e "\033[1;36m"$DIR "->" $HOME"\033[m"
+for f in ${DOTFILES[@]}
+do
+    echo .$f
+    EXIST=`ls -aF ${HOME} | grep $f | grep -v @` #リンクじゃない素の同名ファイルを見つける
+    if [ ${EXIST} ] #もしあったらdotをとる
+    then
+        echo "== original file exists! dot of the filename has been removed. =="
+        mv ${HOME}/.$f ${HOME}/$f
+    fi
+    ln -sf ${DIR}/$f ${HOME}/.$f
+done
